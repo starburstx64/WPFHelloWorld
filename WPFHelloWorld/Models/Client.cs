2e7c58a06d5c6ccb33df19f8aa786ffd0237cdbc
David Segovia Ley <starburstx64@gmail.com>
2019-02-17 13:43:37 -0600

    Agregadas las clases de NotifyBase y Client

---
+++ WPFHelloWorld/Models/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFHelloWorld.Models
+{
+    class Client : NotifyBase
+    {
+        private int id;
+        private string name;
+        private string lastname;
+
+        public int Id
+        {
+            get => id;
+
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+                OnPropertyChanged("DisplayName");
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+                OnPropertyChanged("DisplayName");
+            }
+        }
+
+        public string Lastname
+        {
+            get => lastname;
+
+            set
+            {
+                lastname = value;
+                OnPropertyChanged("lastName");
+                OnPropertyChanged("DisplayName");
+            }
+        }
+
+        public string DisplayName { get => Id + "-" + Name + " " + Lastname; }
+    }
+}
---
+++ WPFHelloWorld/Models/NotifyBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace WPFHelloWorld.Models
+{
+    class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
